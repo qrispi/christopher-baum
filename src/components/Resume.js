552315e275e1e6d76d6edabc23a0a94a82ff5c03
Christopher Baum <christopherabaum@gmail.com>
2023-07-24 16:55:19 -0500

    Add Resume component

---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section>
+            <h2>My Resume</h2>
+            <p>Short intro about your experience and skills.</p>
+            <a href="path/to/your/resume.pdf" download>Download My Resume</a>
+        </section>
+    );
+};
+
+export default Resume;
