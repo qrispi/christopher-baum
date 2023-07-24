0ece2db93165cb7329a7630fd3ddb1b9b2d91e8c
Christopher Baum <christopherabaum@gmail.com>
2023-07-24 17:09:41 -0500

    Update Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import '../styles/Header.css';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Your Name</h1>
-            <p>Your Tagline</p>
+        <header className="header">
+            <h1>My Portfolio</h1>
             <nav>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#resume">Resume</a></li>
-                </ul>
+                <a href="/">Home</a>
+                <a href="/projects">Projects</a>
+                <a href="/resume">Resume</a>
             </nav>
         </header>
     );
