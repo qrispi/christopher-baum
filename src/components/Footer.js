b06661fad9df1e6040e0c0bd9528e86a803f83da
Christopher Baum <christopherabaum@gmail.com>
2023-07-24 16:39:57 -0500

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Contact: your-email@example.com</p>
+            <p>
+                <a href="https://www.linkedin.com/in/yourusername">LinkedIn</a> | 
+                <a href="https://github.com/yourusername">GitHub</a>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
