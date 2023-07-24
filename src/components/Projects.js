ec8d2927810ea3449345a20997e9e13f6209266a
Christopher Baum <christopherabaum@gmail.com>
2023-07-24 16:50:23 -0500

    Add Projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section>
+            <h2>My Projects</h2>
+            <div>
+                <h3>Project 1</h3>
+                <p>Short description about Project 1.</p>
+                <a href="https://project1.com">Project Link</a>
+            </div>
+            {/* Additional project sections can be added similarly */}
+        </section>
+    );
+};
+
+export default Projects;
