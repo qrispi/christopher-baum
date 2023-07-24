8d75257493763007bab8c50277829e41d170a0ba
Christopher Baum <christopherabaum@gmail.com>
2023-07-24 16:45:44 -0500

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <main>
+            <h2>Hello, I'm Your Name</h2>
+            <p>I'm a full-stack web developer.</p>
+            <p>Welcome to my personal portfolio website.</p>
+        </main>
+    );
+};
+
+export default Home;
