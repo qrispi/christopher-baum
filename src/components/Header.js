4f323b8dc92554d42ab22172f94008d72d275a16
Christopher Baum <christopherabaum@gmail.com>
2023-07-24 16:39:06 -0500

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Your Name</h1>
+            <p>Your Tagline</p>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#resume">Resume</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
